bf49af1df9a46ade33822185cdefd3eb2ecc9178
MateuszJanduraUszu <Mati4321@interia.pl>
2023-11-19 19:25:18 +0100

    Add tests for `basic_string_view<CharT>`

---
+++ tests/src/main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <unit/string_view.hpp>
+
+int main() {
+    ::testing::InitGoogleTest();
+    return ::RUN_ALL_TESTS();
+}
