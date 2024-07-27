32502e50a087d45111e4cbc504fd53ce13f5ad46
Mateusz Jandura <mjandura03@gmail.com>
2024-07-28 01:06:20 +0200

    Integrate CMake as build system (#7)

--- src/mjstr/api.hpp
+++ src/mjstr/api.hpp
@@ -7,9 +7,9 @@
 #ifndef _MJSTR_API_HPP_
 #define _MJSTR_API_HPP_
 
-#ifdef MJSTR_EXPORTS
+#ifdef _MJSTR_BUILD
 #define _MJSTR_API __declspec(dllexport)
-#else // ^^^ MJSTR_EXPORTS ^^^ / vvv !MJSTR_EXPORTS vvv
+#else // ^^^ _MJSTR_BUILD ^^^ / vvv !_MJSTR_BUILD vvv
 #define _MJSTR_API __declspec(dllimport)
-#endif // MJSTR_EXPORTS
+#endif // _MJSTR_BUILD
 #endif // _MJSTR_API_HPP_
