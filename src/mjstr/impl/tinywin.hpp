43414c7d10dced8a9d7bc703d3aa6ccdef25f853
MateuszJanduraUszu <Mati4321@interia.pl>
2024-09-02 10:18:08 +0200

    Enable `<Windows.h>` only on Windows

--- src/mjstr/impl/tinywin.hpp
+++ src/mjstr/impl/tinywin.hpp
@@ -7,7 +7,9 @@
 #ifndef _MJSTR_IMPL_TINYWIN_HPP_
 #define _MJSTR_IMPL_TINYWIN_HPP_
 
+#ifdef _MJX_WINDOWS
 #define WIN32_LEAN_AND_MEAN
 #define NOSERVICE
 #include <Windows.h>
+#endif // _MJX_WINDOWS
 #endif // _MJSTR_IMPL_TINYWIN_HPP_
